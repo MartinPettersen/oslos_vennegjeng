5f3eee98d3f92b2376cc0a4a31aef561bbe268f8
MartinPettersen <searchingtheskyfor@gmail.com>
2024-03-07 11:22:20 +0100

    remove scrollbar

--- app/components/(forum)/ContainerForum.tsx
+++ app/components/(forum)/ContainerForum.tsx
@@ -47,7 +47,7 @@ function ContainerForum() {
     <div className="flex flex-col gap-4  w-[100%] sm:w-[70%] h-[50%] sm:h-[66%]  items-center justify-center">
       <ForumSelector forums={forums} />
       {winReady ? (
-        <div className="flex flex-col gap-4 w-[100%] h-[100%] items-center justify-start overflow-y-auto">
+        <div className="flex flex-col gap-4 w-[100%] h-[100%] items-center  no-scrollbar justify-start overflow-y-auto">
           {forums.map((forum) => (
             <ForumDisplay forum={forum} />
           ))}
